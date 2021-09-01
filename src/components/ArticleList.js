15a419297a16ef06c2472fb2220ffe0a36749bbb
Zeon <zeonstewart23@gmail.com>
2021-08-31 20:41:40 -0400

    completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Article from "../components/Article";
+
+function ArticleList ({posts}){
+    console.log(posts)
+    const articles = posts.map((post)=>(
+        <Article
+        key={post.id} 
+        title={post.title} 
+        date={post.date} 
+        preview={post.preview}
+        minutes={post.minutes}
+        />
+
+    ))
+    return(
+        <main>
+            {articles}
+        </main>
+    )
+}
+
+export default ArticleList;
